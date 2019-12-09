643dd83cb87117c6f6e2b56f19ef0e025ac71833
Ryan White <mrwhiterk@gmail.com>
2019-12-09 15:09:01 -0500

    added nav and login component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import LoginForm from './Components/LoginForm/LoginForm'
+import Navbar from './Components/Navigation/Navbar/Navbar'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <LoginForm />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/Components/LoginForm/LoginForm.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import './LoginForm.css'
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <form className="LoginForm">
+        <h2>Login</h2>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            placeholder="Password"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    )
+  }
+}
+
+export default LoginForm
