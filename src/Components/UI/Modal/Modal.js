f672bfd64c77381204f8dfede40131c910a06e8d
Ryan White <mrwhiterk@gmail.com>
2019-12-11 15:14:34 -0500

    finished styling menu modal

--- src/Components/UI/Modal/Modal.js
+++ src/Components/UI/Modal/Modal.js
@@ -1,47 +1,76 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Modal, Button } from 'react-bootstrap'
 
-const modal = props => {
-  let options = []
-
-  for (let i = 2; i <= 10; i++) {
-    options.push(<option value={i}>{i}</option>)
+class MenuModal extends Component {
+  state = {
+    quantity: 1
   }
 
-  return (
-    <Modal show={props.show} onHide={props.handleClose}>
-      <Modal.Header closeButton>
-        <Modal.Title>Add to Cart</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <div className="row">
-          <div className="col-12">
-            Do you want to add the following to your order:
-          </div>
-          <div className="col-10 d-flex text-center align-items-center justify-content-between">
-            <div>{props.itemName}</div> 
-            <div>$ {props.itemPrice.toFixed(2)}</div>
-          </div>
-          <div className="col-2">
-            <div class="input-group">
-              <select class="custom-select" id="inputGroupSelect04">
-                <option selected>1</option>
-                {options}
-              </select>
+  setQuantity = e => {
+    this.setState({ quantity: e.target.value })
+  }
+
+  render() {
+    let options = []
+
+    for (let i = 2; i <= 10; i++) {
+      options.push(
+        <option value={i} key={i}>
+          {i}
+        </option>
+      )
+    }
+
+    return (
+      <Modal show={this.props.show} onHide={this.props.handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add to Cart</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="row">
+            <div className="col-12">
+              Do you want to add the following to your order:
+            </div>
+            <div className="col-8 d-flex text-center align-items-center justify-content-between">
+              <div>{this.props.itemName}</div>
+              <div>$ {this.props.itemPrice.toFixed(2)}</div>
+            </div>
+            <div className="col-4">
+              <div class="input-group">
+                <select
+                  class="custom-select"
+                  id="inputGroupSelect04"
+                  onChange={this.setQuantity}
+                >
+                  <option>0</option>
+                  <option selected>1</option>
+                  {options}
+                </select>
+              </div>
             </div>
           </div>
-        </div>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={props.handleClose}>
-          Close
-        </Button>
-        <Button variant="success" onClick={props.handleSubmit}>
-          Add to Cart
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  )
+        </Modal.Body>
+        <Modal.Footer>
+          <div className="d-flex justify-content-between row w-100 align-items-center">
+            <div>
+              Total: $ {(this.props.itemPrice * this.state.quantity).toFixed(2)}
+            </div>
+            <div>
+              <Button variant="secondary" onClick={this.props.handleClose}>
+                Close
+              </Button>
+
+              {this.state.quantity > 0 && (
+                <Button variant="success" onClick={this.props.handleSubmit}>
+                  Add to Cart
+                </Button>
+              )}
+            </div>
+          </div>
+        </Modal.Footer>
+      </Modal>
+    )
+  }
 }
 
-export default modal
+export default MenuModal
