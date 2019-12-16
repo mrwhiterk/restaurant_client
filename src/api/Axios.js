6be783c6f20dcf1972b4ffb20f6de250ac8f68af
Ryan White <mrwhiterk@gmail.com>
2019-12-16 13:50:29 -0500

    fixed url prod/dev links

--- src/api/Axios.js
+++ src/api/Axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 console.log(process.env.NODE_ENV)
 export const Axios = axios.create({
   baseURL:
-    process.env.NODE_ENV === 'production'
+    process.env.NODE_ENV !== 'production'
       ? 'http://localhost:3001'
       : 'https://ryan-orderapp.herokuapp.com'
 })
