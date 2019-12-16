4a1166c1a923469eced723e8b13af742d95db883
Ryan White <mrwhiterk@gmail.com>
2019-12-16 12:06:50 -0500

    Updates

---
+++ precache-manifest.7a4a5ebebf62f1876b8cdc0a2e02317e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "670e787624c3f2d2591fb0d8b63f14b4",
+    "url": "/restaurant-app/index.html"
+  },
+  {
+    "revision": "a4165768ac854d42498a",
+    "url": "/restaurant-app/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "2afc6fbd6fb4fc329a1f",
+    "url": "/restaurant-app/static/css/main.29bb4c55.chunk.css"
+  },
+  {
+    "revision": "a4165768ac854d42498a",
+    "url": "/restaurant-app/static/js/2.4123723f.chunk.js"
+  },
+  {
+    "revision": "3628f5b9f01ff204ba3fe2781466a3d6",
+    "url": "/restaurant-app/static/js/2.4123723f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2afc6fbd6fb4fc329a1f",
+    "url": "/restaurant-app/static/js/main.81844fd8.chunk.js"
+  },
+  {
+    "revision": "2596f03661685780adca",
+    "url": "/restaurant-app/static/js/runtime-main.9e1f7ff4.js"
+  }
+]);
